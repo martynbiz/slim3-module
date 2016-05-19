42fc81e94e9ee8df088deb218fcf401fc59001e5
Martyn Bissett <martynbissett@yahoo.co.uk>
2016-05-19 11:38:51 +0900

    Add settings

--- src/Module.php
+++ src/Module.php
@@ -11,9 +11,15 @@ class Module
      */
     protected $app;
 
-    public function __construct($app)
+    /**
+     * @var array
+     */
+    protected $settings;
+
+    public function __construct($app, $settings=array())
     {
         $this->app = $app;
+        $this->settings = $settings;
     }
 
     /**
@@ -23,43 +29,10 @@ class Module
     public function load($moduleName)
     {
         // get the path of the modules
-        $modulePath = realpath(APPLICATION_PATH . '/modules/' . $moduleName);
+        $modulePath = realpath($this->settings['modules_dir'] . '/' . $moduleName);
 
         // include the required module.php path of that module
         $app = $this->app;
         require $modulePath . '/module.php';
     }
-
-    // /**
-    //  * This will return a closure that will only run when the module is required
-    //  * when a route matches. It can be used to load module config
-    //  * @param string $moduleName
-    //  * @return \Closure
-    //  */
-    // public function __invoke($moduleName)
-    // {
-    //     $app = $this->app;
-    //
-    //     $callable = function ($request, $response, $next) use ($app, $moduleName) {
-    //
-    //         $container = $app->getContainer();
-    //
-    //         // anything within here will only run for the module that is
-    //         // attached to the matching route (e.g. config/module.php)
-    //
-    //         // // merge config of this with app
-    //         // $modulePath = realpath(APPLICATION_PATH . '/modules/' . $moduleName);
-    //         // $moduleSettingsPath = $modulePath . '/config/module.php';
-    //         // if (file_exists($moduleSettingsPath)) {
-    //         //     $moduleSettings = require $moduleSettingsPath;
-    //         // }
-    //         // $container['settings']->__construct($moduleSettings);
-    //
-    //         $response = $next($request, $response);
-    //
-    //         return $response;
-    //     };
-    //
-    //     return $callable;
-    // }
 }
