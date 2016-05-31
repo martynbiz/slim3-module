3f28013f2a24b496c87618308f6019a4df34e9d4
Martyn Bissett <m@rtyn.biz>
2016-05-31 21:43:47 +0900

    Add autoloader to module loader

--- src/Module.php
+++ src/Module.php
@@ -1,6 +1,8 @@
 <?php
 namespace MartynBiz\Slim3Module;
 
+use Composer\Autoload\ClassLoader;
+
 /**
  * This will load modules
  */
@@ -12,14 +14,35 @@ class Module
     protected $app;
 
     /**
+<<<<<<< Updated upstream
+=======
+     * @var ClassLoader
+     */
+    protected $classLoader;
+
+    /**
+>>>>>>> Stashed changes
      * @var array
      */
     protected $settings;
 
+<<<<<<< Updated upstream
     public function __construct($app, $settings=array())
     {
         $this->app = $app;
         $this->settings = $settings;
+=======
+    public function __construct($app, ClassLoader $classLoader, $settings=array())
+    {
+        $this->app = $app;
+        $this->classLoader = $classLoader;
+        $this->settings = $settings;
+
+        // load each module in $settings
+        foreach($settings['autoload'] as $module) {
+            $this->load($module);
+        }
+>>>>>>> Stashed changes
     }
 
     /**
@@ -29,10 +52,23 @@ class Module
     public function load($moduleName)
     {
         // get the path of the modules
+<<<<<<< Updated upstream
         $modulePath = realpath($this->settings['modules_dir'] . '/' . $moduleName);
+=======
+        $modulesPath = $this->settings['modules_path'];
+>>>>>>> Stashed changes
+
+        // we want these items to be available to module.php
+        $app = $this->app;
+<<<<<<< Updated upstream
+        require $modulePath . '/module.php';
+=======
+        $classLoader = $this->classLoader;
+        $container = $this->app->getContainer();
 
         // include the required module.php path of that module
-        $app = $this->app;
-        require $modulePath . '/module.php';
+        $moduleClassPath = sprintf('%s/%s/module.php', $modulesPath, $moduleName);
+        require $moduleClassPath;
+>>>>>>> Stashed changes
     }
 }
