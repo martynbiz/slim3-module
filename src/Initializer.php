687976586d4b62b567bc1d88a0d6ad8dc98856ea
Martyn Bissett <m@rtyn.biz>
2016-06-09 23:09:17 +0900

    Tidy up

--- src/Initializer.php
+++ src/Initializer.php
@@ -18,44 +18,24 @@ class Initializer
      */
     protected $app;
 
-    // /**
-    //
-    //  * @var ClassLoader
-    //  */
-    // protected $classLoader;
-
     /**
      * @var array
      */
-    protected $settings;
+    protected $initializerSettings;
 
     /**
      * @var array
      */
     protected $moduleInstances = [];
 
-    public function __construct($app, $settings=array())
+    public function __construct($app, $modules=array())
     {
         $this->app = $app;
-        // $this->classLoader = $classLoader;
-        $this->settings = $settings;
 
         // build an class map of [[module => moduleClassPath], ..]
-        // $moduleClassMap = [];
-        foreach ($this->settings['autoload'] as $moduleName) {
-
-            // // e.g. "/path/to/modules/{module_name}/Module.php"
-            // $moduleClassPath = realpath(sprintf('%s/%s/Module.php', $this->settings['modules_path'], $moduleName));
-            // if (! $moduleClassPath) {
-            //     throw new \Exception('Module class file could not be found');
-            // }
-
-            // // include the required module.php path of that module
-            // require $moduleClassPath;
-
-            // add to class map so we can easily access it later in this function
-            $moduleClassName = sprintf('%s\Module', $moduleName);
-            $this->moduleInstances[$moduleName] = new $moduleClassName();
+        foreach ($modules as $module) {
+            $moduleClassName = sprintf('%s\Module', $module);
+            $this->moduleInstances[$module] = new $moduleClassName();
         }
     }
 
@@ -66,39 +46,15 @@ class Initializer
     public function initModules()
     {
         $moduleInstances = $this->moduleInstances;
-        // $classLoader = $this->classLoader;
         $app = $this->app;
         $container = $app->getContainer();
 
-        // next, load settings of all modules
-        foreach ($moduleInstances as $moduleName => $module) {
-            $allSettings = $container['settings']->all();
-            if (!isset($allSettings['modules'][$moduleName]) or !is_array($allSettings['modules'][$moduleName])) {
-                $allSettings['modules'][$moduleName] = [];
-            }
-            $allSettings['modules'] = array_merge_recursive($allSettings['modules'], $this->getModuleConfig());
-            $container['settings']->__construct( $allSettings );
-        }
-
         // $this->initClassLoader($classLoader);
+        $this->initModuleConfig();
         $this->initDependencies($container);
         $this->initMiddleware($app);
         $this->initRoutes($app);
     }
-
-    // /**
-    //  * Load the module. This will run for all modules, use for routes mainly
-    //  * @param string $moduleName Module name
-    //  */
-    // public function initClassLoader()
-    // {
-    //     $moduleInstances = $this->moduleInstances;
-    //     $classLoader = $this->classLoader;
-    //
-    //     foreach ($moduleInstances as $module) {
-    //         $module->initClassLoader($classLoader);
-    //     }
-    // }
 
     /**
      * Load the module. This will run for all modules, use for routes mainly
@@ -115,6 +71,24 @@ class Initializer
         }
 
         return $allModules;
+    }
+
+    /**
+     * Load the module. This will run for all modules, use for routes mainly
+     * @param string $moduleName Module name
+     */
+    public function initModuleConfig()
+    {
+        $app = $this->app;
+        $container = $app->getContainer();
+
+        $allSettings = $container['settings']->all();
+        if (!isset($allSettings['modules']) or !is_array($allSettings['modules'])) {
+            $allSettings['modules'] = [];
+        }
+
+        $allSettings['modules'] = array_merge_recursive($allSettings['modules'], $this->getModuleConfig());
+        $container['settings']->__construct( $allSettings );
     }
 
     /**
